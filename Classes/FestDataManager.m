futurice/festapp-ios

initWithDictionary, also fix a typo

Classes/FestDataManager.m
@@ -153,7 +153,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = gigsArray[idx];
-        Gig *gig = [[Gig alloc] initFromJSON:obj];
+        Gig *gig = [[Gig alloc] initWithDictionary:obj];
         if (gig) {
             [gigs addObject:gig];
         }
@@ -170,7 +170,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = newsArray[idx];
-        NewsItem *item = [[NewsItem alloc] initFromJSON:obj];
+        NewsItem *item = [[NewsItem alloc] initWithDictionary:obj];
         if (item) {
             [news addObject:item];
         }
@@ -191,7 +191,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = infoArray[idx];
-        NewsItem *item = [[NewsItem alloc] initFromJSON:obj];
+        InfoItem *item = [[InfoItem alloc] initWithDictionary:obj];
         if (item) {
             [info addObject:item];
         }

futurice/sec-conference-ios

Conflicts:
	Classes/FestDataManager.m

Classes/FestDataManager.m
@@ -163,7 +163,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = gigsArray[idx];
-        Event *gig = [[Event alloc] initFromJSON:obj];
+        Event *gig = [[Event alloc] initWithDictionary:obj];
         if (gig) {
             [gigs addObject:gig];
         }
@@ -180,7 +180,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = newsArray[idx];
-        NewsItem *item = [[NewsItem alloc] initFromJSON:obj];
+        NewsItem *item = [[NewsItem alloc] initWithDictionary:obj];
         if (item) {
             [news addObject:item];
         }
@@ -201,7 +201,7 @@
 
     for (NSUInteger idx = 0; idx < len; idx++) {
         NSDictionary *obj = infoArray[idx];
-        NewsItem *item = [[NewsItem alloc] initFromJSON:obj];
+        InfoItem *item = [[InfoItem alloc] initWithDictionary:obj];
         if (item) {
             [info addObject:item];
         }